524d5a9d982801de84fc8633d4f913be34f49754
abimutholib504 <abimutholib3003@gmail.com>
2024-12-03 23:57:52 +0700

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,23 +38,64 @@ class DatabaseSeeder extends Seeder
             'isAdmin' => false,
         ]);
 
+        User::create([
+            'name' => 'Yudha',
+            'email' => 'yudha@gmail.com',
+            'password' => Hash::make('12345678'),
+            'isAdmin' => false,
+        ]);
+
+        User::create([
+            'name' => 'Faris',
+            'email' => 'faris@gmail.com',
+            'password' => Hash::make('12345678'),
+            'isAdmin' => false,
+        ]);
+
         // Seed profiles
         Profile::create([
-            'npm' => 'admin',
-            'prodi' => 'admin',
-            'alamat' => 'kampus',
-            'noTelp' => 'admin',
+            'npm' => '19990020',
+            'prodi' => 'Administrator',
+            'alamat' => 'Dimana Bumi Dipijak',
+            'noTelp' => '08123456789',
             'users_id' => 1,
         ]);
 
         Profile::create([
             'npm' => '17207024',
             'prodi' => 'Teknik Informatika',
-            'alamat' => 'Jakarta',
-            'noTelp' => '089609760132',
+            'alamat' => 'Jakarta Barat',
+            'noTelp' => '085171502026',
             'users_id' => 2,
+
         ]);
 
+        Profile::create([
+            'npm' => '17215114',
+            'prodi' => 'Teknik Informatika',
+            'alamat' => 'Citayam',
+            'noTelp' => '083819379569',
+            'users_id' => 3,
+
+        ]);
+
+        Profile::create([
+            'npm' => '17215012',
+            'prodi' => 'Teknik Informatika',
+            'alamat' => 'Jakarta Pusat',
+            'noTelp' => '085697713966',
+            'users_id' => 4,
+
+        ]);
+
+        Profile::create([
+            'npm' => '17207025',
+            'prodi' => 'Teknik Informatika',
+            'alamat' => 'Jakarta Timur',
+            'noTelp' => '085156166032',
+            'users_id' => 5,
+
+        ]);
         // Seed categories
         Kategori::create(['nama' => 'Novel', 'deskripsi' => 'Kumpulan Novel']);
         Kategori::create(['nama' => 'Pelajaran', 'deskripsi' => 'Kumpulan Buku materi pelajaran']);
